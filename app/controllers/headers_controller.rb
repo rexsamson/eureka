d9679e37724103a1ad514cea16dc64822bbe8820
Rex Samson <rexsamso@gmail.com>
2017-04-22 09:21:31 +0000

    edit action

--- app/controllers/headers_controller.rb
+++ app/controllers/headers_controller.rb
@@ -4,19 +4,33 @@ class HeadersController < ApplicationController
       @header = Header.find(params[:id])
    end
    
+   def edit
+      @header = Header.find(params[:id])
+   end
+   
    def new 
      @header = Header.new
    end
    
    def create
       @header = Header.new(header_params)
-      if 
-      @header.save
-      flash[:notice] = "Data created successfully!"
-      redirect_to header_path(@header)
+      if @header.save
+         flash[:notice] = "Data created successfully!"
+         redirect_to header_path(@header)
       else
          render 'new'
       end
+   end
+   
+   def update
+      @header = Header.find(params[:id])
+      if @header.update(header_params)
+         flash[:notice] = "Data updated"
+         redirect_to header_path(@header)
+      else
+         render 'edit'
+      end
+         
    end
    
    private 
