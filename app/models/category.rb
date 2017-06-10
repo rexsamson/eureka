b4491da4ff870c1132128daa95b0368214090aaf
Rex Samson <rexsamso@gmail.com>
2017-06-10 07:35:21 +0000

    610.2 update

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
     extend FriendlyId
     friendly_id :name, use: :slugged
-    has_many :products, inverse_of: :category, :dependent => :restrict
+    has_many :products, inverse_of: :category, :dependent => :restrict_with_error
     belongs_to :branch, inverse_of: :categories
     NON_VALIDATABLE_ATTRS = ["id"]
     VALIDATABLE_ATTRS = Category.attribute_names.reject{|attr| NON_VALIDATABLE_ATTRS.include?(attr)}
